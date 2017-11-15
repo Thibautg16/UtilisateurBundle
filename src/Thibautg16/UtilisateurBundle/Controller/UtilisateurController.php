d41a07f1a0543d8a6a24f1ada5cba0a51d43fe68
Thibautg16 <thibaut@tgillardeau.com>
2017-11-15 23:32:56 +0100

    version 0.3

--- src/Thibautg16/UtilisateurBundle/Controller/UtilisateurController.php
+++ src/Thibautg16/UtilisateurBundle/Controller/UtilisateurController.php
@@ -15,85 +15,80 @@ use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 use Thibautg16\UtilisateurBundle\Entity\Utilisateur;
+use Thibautg16\UtilisateurBundle\Form\UtilisateurType;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
 class UtilisateurController extends Controller{
 
-        public function listeAction(){
+        /**
+         * @Security("has_role('ROLE_UTILISATEUR_ADMIN')")
+         */
+        public function listerAction(){
                 $em = $this->getDoctrine()->getManager();
-                // On vérifie si l'utilisateur (via les groupes) est autorisé à consulter cette page
-                if($em->getRepository('Thibautg16UtilisateurBundle:Groupe')->GroupeAutoriseRoute($this->getUser(), $this->container->get('request')->get('_route')) == TRUE){
-                        /****** On liste tous les services ******/
-                        // On récupère tous les services actuellement en BDD
-                        $listeUtilisateur = $em
-                                ->getRepository('Thibautg16UtilisateurBundle:Utilisateur')
-                                ->findAll()
-                        ;
+                $listeUtilisateur = $em->getRepository('UtilisateurBundle:Utilisateur')->findAll();
 
-                        return $this->render('Thibautg16UtilisateurBundle:Utilisateur:liste.html.twig', array('listeUtilisateur' => $listeUtilisateur));
-                }
-                // Ici, $user est une instance de notre classe User mais n'est pas Admin
-                else{
-                        return $this->redirect($this->generateUrl('thibautg16_utilisateur_homepage'));
-                }
-    }
+                return $this->render('UtilisateurBundle:Utilisateur:lister.html.twig', array('listeUtilisateur' => $listeUtilisateur));
+        }
 
+        /**
+         * @Security("has_role('ROLE_UTILISATEUR_ADMIN')")
+         */        
         public function ajouterAction(Request $request){
                 $em = $this->getDoctrine()->getManager();
-                // On vérifie si l'utilisateur (via les groupes) est autorisé à consulter cette page
-                if($em->getRepository('Thibautg16UtilisateurBundle:Groupe')->GroupeAutoriseRoute($this->getUser(), $this->container->get('request')->get('_route')) == TRUE){
-                        // Création de l'objet Services
-                        $oUser = new Utilisateur();
 
-                        // On crée le FormBuilder grâce au service form factory
-                        $formBuilder = $this->get('form.factory')->createBuilder('form', $oUser);
+                // Création de l'objet
+                $oUtilisateur = new Utilisateur();
 
-                        // On ajoute les champs de l'entité que l'on veut à notre formulaire
-                        $formBuilder
-                          ->add('username',     'text')
-                          ->add('email',        'text')
-                          ->add('password', 'repeated', array('first_name' => 'password', 'second_name' => 'confirm','type' => 'password' ))
-                          ->add('Active',       'checkbox', array('required' => false))
-                          ->add('Groupes', 'entity', array('class' => 'Thibautg16UtilisateurBundle:Groupe', 'property' => 'nom', 'multiple' => true))
-                          ->add('Ajouter',      'submit')
-                        ;
+                // On crée le FormBuilder grâce au service form factory
+                $form = $this->createForm(UtilisateurType::class, $oUtilisateur);    
 
-                        // À partir du formBuilder, on génère le formulaire
-                        $form = $formBuilder->getForm();
+                // On ajoute les champs de l'entité que l'on veut à notre formulaire
+                $form
+                        ->add('username', TextType::class)
+                        ->add('email',    TextType::class)
+                        ->add('password', RepeatedType::class, array('first_name' => 'password', 'second_name' => 'confirm','type' => PasswordType::class))
+                        ->add('Active',   CheckboxType::class, array('required' => false))
+                        ->add('Groupes',  EntityType::class, array('class' => 'Thibautg16UtilisateurBundle:Groupe', 'property' => 'nom', 'multiple' => true))
+                        ->add('Ajouter',  SubmitType::class)
+                ;
 
-                        // On fait le lien Requête <-> Formulaire
-                        $form->handleRequest($request);
 
-                        // On vérifie que les valeurs entrées sont correctes
+                // On fait le lien Requête <-> Formulaire
+                $form->handleRequest($request);
+
+                // On vérifie que les valeurs entrées sont correctes
+                if ($form->isSubmitted()) {
                         if ($form->isValid()) {
 
-                          // On enregistre notre objet $oServices dans la base de données
-                          $em = $this->getDoctrine()->getManager();
-                          //$oUser->setCreePar($user);
+                        // On enregistre notre objet $oServices dans la base de données
+                        $em = $this->getDoctrine()->getManager();
+                        //$oUtilisateur->setCreePar($user);
                           
-                          $factory = $this->container->get('security.encoder_factory');                              
-                          $encoder = $factory->getEncoder($oUser);
-                          $password = $encoder->encodePassword($oUser->getPassword(), $oUser->getSalt());
-                          $oUser->setPassword($password);
-                          
-			  $oUser->setRoles('ROLE_ADMIN'); 
+                        $factory = $this->container->get('security.encoder_factory');                              
+                        $encoder = $factory->getEncoder($oUtilisateur);
+                        $password = $encoder->encodePassword($oUtilisateur->getPassword(), $oUtilisateur->getSalt());
+                        $oUtilisateur->setPassword($password);                          
+			$oUtilisateur->setRoles('ROLE_UTILISATEUR'); 
 
-                          $em->persist($oUser);
-                          $em->flush();
+                        $em->persist($oUtilisateur);
+                        $em->flush();
 
-                          $request->getSession()->getFlashBag()->add('success', 'Création du compte : '.$oUser->getUsername().' effectuée avec succès.');
+                        $request->getSession()->getFlashBag()->add('success', 'Création du compte : '.$oUtilisateur->getUsername().' effectuée avec succès.');
 
-                          // On redirige vers la page de visualisation de l'annonce nouvellement créée
-                          return $this->redirect($this->generateUrl('thibautg16_utilisateur_liste'));
-                        }
+                        // On redirige vers la page de visualisation de l'annonce nouvellement créée
+                        return $this->redirect($this->generateUrl('thibautg16_utilisateur_lister'));
+                }
 
-                        // À ce stade, le formulaire n'est pas valide car :
-                        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-                        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
-                        return $this->render('Thibautg16UtilisateurBundle:Utilisateur:ajouter.html.twig', array(
-                          'form' => $form->createView(), 'utilisateur' => $oUser));
+                // Le formulaire n'est pas valide, donc on l'affiche de nouveau
+                return $this->render('Thibautg16UtilisateurBundle:Utilisateur:ajouter.html.twig', array('form' => $form->createView(), 'utilisateur' => $oUtilisateur));
                 }
                 // Ici, $user est une instance de notre classe User mais n'est pas Admin
                 else{
--- src/Thibautg16/UtilisateurBundle/Thibautg16UtilisateurBundle.php
+++ src/Thibautg16/UtilisateurBundle/Thibautg16UtilisateurBundle.php
@@ -4,6 +4,5 @@ namespace Thibautg16\UtilisateurBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class Thibautg16UtilisateurBundle extends Bundle
-{
+class Thibautg16UtilisateurBundle extends Bundle{
 }
